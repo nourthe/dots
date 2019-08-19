b714002e0d71b889d279b8909fe3183fdebe5533
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-08-19 18:14:07 -0300

    Moved color vars to prompt.bash.

--- .bash/colors.bash
+++ .bash/colors.bash
@@ -1,8 +1,3 @@
-# Color variables
-GREEN="\[$(tput setaf 2)\]"
-BLUE="\[$(tput setaf 4)\]"
-RESET="\[$(tput sgr0)\]"
-
 # Color aliases
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -1,1 +1,8 @@
+# Prompt Color Variables
+BOLD="\[$(tput bold)\]"
+GREEN="\[$(tput setaf 2)\]"
+BLUE="\[$(tput setaf 4)\]"
+RESET="\[$(tput sgr0)\]"
+
+# Prompt
 export PS1="[${GREEN}\u@\h ${BLUE}\W${RESET}]\$"
