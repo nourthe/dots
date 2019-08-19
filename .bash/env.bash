65959d0083f010c7cc0b9084e9ae50169f503bb1
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-08-19 18:13:18 -0300

    Changed default browser to google-chrome.

--- .bash/env.bash
+++ .bash/env.bash
@@ -2,6 +2,6 @@ export EDITOR=vim
 export PAGER=less
 
 # Set the default browser.
-if [ -x $(command -v firefox) ]; then
-    export BROWSER=firefox  # firefox
+if [ -x $(command -v google-chrome-stable) ]; then
+    export BROWSER=google-chrome-stable  # google-chrome
 fi
