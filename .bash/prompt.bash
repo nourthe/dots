2c106132e69e3fa63218f9dded35ee707887be46
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2019-11-01 00:42:15 -0300

    Updating prompt design.

--- .bash/prompt.bash
+++ .bash/prompt.bash
@@ -1,8 +1,9 @@
 # Prompt Color Variables
-BOLD="\[$(tput bold)\]"
-GREEN="\[$(tput setaf 2)\]"
-BLUE="\[$(tput setaf 4)\]"
-RESET="\[$(tput sgr0)\]"
+BOLD="$(tput bold)"
+GREEN="$(tput setaf 2)"
+BLUE="$(tput setaf 4)"
+CIAN="$(tput setaf 6)"
+RESET="$(tput sgr0)"
 
 # Prompt
-export PS1="[${GREEN}\u@\h ${BLUE}\W${RESET}]\$"
+export PS1="${BOLD}${GREEN}[\u@\h ${CIAN}\W${GREEN}]\$${RESET} "
