932dcc5f659a78cce67faa1413b4fe32091bb558
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2021-04-19 10:37:59 -0300

    Iniciando en nahuarch nuevas configs

--- .dots_init.sh
+++ .dots_init.sh
@@ -4,3 +4,5 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+yay -S ttf-meslo-nerd-font-powerlevel10k
