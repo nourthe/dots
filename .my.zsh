8d5119b416ee39d968d883f18cecd1f401886945
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2021-04-19 13:45:59 -0300

    Finalizando configuracion urxvt y zsh

---
+++ .my.zsh
@@ -0,0 +1,10 @@
+# Colors
+[[ -f $HOME/.bash/prompt.bash ]] && source $HOME/.bash/prompt.bash
+
+# Welcome
+echo ${BLUE}╔══╗
+echo ${BLUE}╚╗╔╝       		    	 ${BOLD}${BLUE}Bienvenido a ${RED}$HOST${BLUE} $USERNAME.${RESET}
+echo ${BLUE}╔╝${BOLD}${RED}\(\¯\`v\´\¯\)${RESET}
+echo ${BLUE}╚══${BOLD}${RED}\`\.\¸\.${VIOLET}\<\/CODE\>
+
+alias dots='/usr/bin/git --git-dir=/home/nahuel/.dots/ --work-tree=/home/nahuel'
--- .zshrc
+++ .zshrc
@@ -118,7 +118,7 @@ source $ZSH/oh-my-zsh.sh
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-source ~/.myzshrc
+[[ ! -f ~/.my.zsh ]] || source ~/.my.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
