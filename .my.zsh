31df0fbcf120b11353a862302622fdd65db20e9b
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2021-06-01 10:49:02 -0300

    Agrega aliases de seguridad para cp y rm

--- .my.zsh
+++ .my.zsh
@@ -10,3 +10,10 @@ echo ${GREEN}╚══${BOLD}${RED}\`\.\¸\.${VIOLET}\<\/CODE\>
 alias tp='trash-put'
 
 alias dots='/usr/bin/git --git-dir=/home/nahuel/.dots/ --work-tree=/home/nahuel'
+alias cp='cp -i'
+alias rm='trash-put'
+alias tm='trash-put'
+# errors control
+alias fuck='sudo $(history -p \!\!)'
+alias :q!='exit'
+alias :q='exit'
