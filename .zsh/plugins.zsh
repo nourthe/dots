4a8e80d5ae684ddd251634e7127b30b4fb8f9a40
Nahuel Ourthe <nahuelourthe1996@gmail.com>
2022-05-28 00:37:07 -0300

    Moving to zsh antigen plugin manager.

---
+++ .zsh/plugins.zsh
@@ -0,0 +1,19 @@
+antigen use oh-my-zsh
+
+antigen bundle git
+
+# Syntax Highlighting
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Auto suggestions
+antigen bundle zsh-users/zsh-autosuggestions
+
+# p10k theme
+antigen theme romkatv/powerlevel10k
+
+# Improved Vi mode
+antigen bundle jeffreytse/zsh-vi-mode
+
+###
+# Tell Antigen that you're done.
+antigen apply
